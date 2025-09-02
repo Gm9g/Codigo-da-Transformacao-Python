017baf0fd70c8768bbbd708cec0bd5231201dfa4
Gabriel-em-Vocação <00001118493928sp@al.educacao.sp.gov.br>
2025-09-02 02:39:05 -0300

    Core matemática

---
+++ Modulo-11/core.py
@@ -0,0 +1,15 @@
+# core/matematica.py
+
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a / b
